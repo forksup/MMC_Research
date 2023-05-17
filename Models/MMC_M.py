e4c9388b9924ec14e6134665ca07ff1f2c4463cb
mitchellbucklew <mbucklew@asu.edu>
2023-05-17 16:14:15 -0700

    Fixed MMC_M class

--- Models/MMC_M.py
+++ Models/MMC_M.py
@@ -1,28 +1,15 @@
 from enum import Enum
-from Models.MMC import MMC
+from Models.MMC import MMC, sgo_types
 
-
-class sgo_types(Enum):
-    greedy = 1
-    hillclimb = 2
-    full = 3
-    geometric_mean = 4
-
-
-class MMC(MMC):
+# An MMC model with combining misalignment feature disabled
+class MMC_M(MMC):
     def __init__(
         self,
         state_size,
         order,
         sgo_method: sgo_types = sgo_types.greedy,
-        misalignment=True,
         verbose=False,
     ):
-        self.sgom = sgo_method
-        self.state_size = state_size
-        self.states = [i for i in range(state_size)]
-        self.cpt = None
-        self.SGO = None
-        self.name = "MMC"
-        self.verbose = verbose
-        self.index_dict = {}
+    
+        super(state_size, order, sgo_method, False).__init__()
+
